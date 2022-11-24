2ba5823533a019cabd9b57ed1c37edcc91169cec
smd92 <69214590+smd92@users.noreply.github.com>
2022-11-24 16:45:39 +0100

    2:00:30

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,17 @@ import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
