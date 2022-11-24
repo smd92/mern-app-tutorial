3c285cb5ba4f356af87fb3e1a1f4b99aebdbefd1
smd92 <69214590+smd92@users.noreply.github.com>
2022-11-24 12:53:23 +0100

    1:53:14

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return (
-    <div className="app">
-
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
