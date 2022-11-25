bc70eeb1bb9441aa72183c6cd9c00f8bbcd54ae4
smd92 <69214590+smd92@users.noreply.github.com>
2022-11-25 13:49:55 +0100

    2:19:15

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -22,5 +22,26 @@ export const authSlice = createSlice({
       state.user = null;
       state.token = null;
     },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends; //if user exists, set friends
+      } else {
+        console.error("there are no user friends");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
   },
 });
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
